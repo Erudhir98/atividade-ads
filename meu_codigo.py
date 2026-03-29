e817415a395300f7d7980111ec0292d6e1687948
Gabriel Ferreira <gabrielferreiraerudhir@gmail.com>
2026-03-29 13:49:58 -0300

    Add files via upload
    atividade py

---
+++ meu_codigo.py
@@ -0,0 +1,50 @@
+# multiplicação de dois números
+a = 10
+b = 2
+print(a * b)
+
+# média de cinco números
+
+a = 10
+b = 5
+c = 15
+d = 6
+e = 3
+media = (a + b + c + d + e) / 5
+print("media: ", media)
+
+# preço final com acrescimo de 8%
+
+a = 75
+final = a + (a * 0.08)
+print("final: ", final)
+
+# subtração de dois números
+
+a = 40
+b = 23
+print(a - b)
+
+# cálculo IMC
+
+peso = 80
+altura = 1.80
+imc = peso / (altura * altura)
+print("imc: ", imc)
+
+# conversão de medida de temperatura
+
+celcius = 29
+fahrenheit = (celcius * 9 / 5) + 32
+print("fahrenheit: ", fahrenheit)
+
+# salário final
+
+horas = 8
+valorHora = 10
+salario = horas * valorHora
+print("salario: ", salario)
+
+
+ 
+ 
